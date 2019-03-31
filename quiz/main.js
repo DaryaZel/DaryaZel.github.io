b9a6db19a4f74aec36a35de9f4570b3855101511
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2019-03-31 12:35:56 +0300

    Update main.js

--- quiz/main.js
+++ quiz/main.js
@@ -3,30 +3,20 @@ let box = document.getElementById("box"),
     myResult = document.getElementById("result__contain")
 
 
-//функция по подсчету весов выбранных ответов и вывод результата
 function showResults(box) {
-    debugger
-    //коллекция из всех вариантов ответов
     let answerContainers = box.querySelectorAll('.answers');
     debugger
     let userAnswers = [];
-    //теперь нужно найти выбранные ответы 
-    for (var i = 0; i < answerContainers.length; i++) {
-        //проверяем какие ответы checked 
+    for (var i = 0; i < answerContainers.length; i++) { 
         let userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked'))
-        //если не все ответы выбраны выскакивает просьба заполнить все ответы
         if (userAnswer === null) {
-
             return alert("Ответьте на все вопросы!");
-        }
-        //если все ответы выбраны присваиваем перемменной uderAnswer вес ответа и создаем массив из весов ответов  
+        }  
         else {
             let userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked')).value;
             userAnswers.push(userAnswer)
         }
-
     }
-    //определяем какой вариант весов встречается чаще
     function maxValue(array) {
         debugger
         let obj = {};
@@ -44,7 +34,6 @@ function showResults(box) {
     }
     return maxValue(userAnswers)
 }
-//кликом по баттону выводим результат
 submition.addEventListener('click', () => {
     showResults(box);
 });
