a87b0d3732800e0dc6b352720ee2fa2eece9a34a
DaryaZel <40724895+DaryaZel@users.noreply.github.com>
2019-04-04 01:58:08 +0300

    Add files via upload

---
+++ calculator/main.js
@@ -0,0 +1,57 @@
+﻿let num = document.querySelectorAll('.num'),
+    operator = document.querySelectorAll('.operator'),
+    display = document.querySelector('.calculator__display'),
+    clear = document.querySelector('.clear'),
+    clearAll = document.querySelector('.clearAll'),
+    equal = document.querySelector('.equal'),
+    save = document.querySelector('.save')
+
+for (i = 0; i < num.length; i++) {
+    let numbers = num[i]
+    numbers.addEventListener('click', (e) => {
+        let numberValue = e.target.value
+        insert(numberValue)
+    })
+}
+for (i = 0; i < operator.length; i++) {
+    let operators = operator[i]
+    operators.addEventListener('click', (e) => {
+        let operatorValue = e.target.value
+        insert(operatorValue)
+    })
+}
+
+function insert(valueForDisplay) {
+    display.value += valueForDisplay
+}
+
+clear.addEventListener('click', () => {
+    display.value = " "
+})
+clearAll.addEventListener('click', () => {
+    display.value = " "
+    window.localStorage.clear();
+})
+
+equal.addEventListener('click', () => {
+    display.value = eval(display.value)
+})
+
+save.addEventListener('click', () => {
+    if (localStorage.getItem('loggedIn')) {
+        display.value = localStorage.getItem('key');
+    } else {
+        localStorage.setItem('key', (function() {
+            localStorage.setItem('loggedIn', 'true');
+            let user = display.value;
+            return user;
+        })());
+    }
+
+})
+
+
+
+if (localStorage.getItem('loggedIn')) {
+    alert(`Результат последнего вычисления равен: ${localStorage.getItem('key')}`)
+}
